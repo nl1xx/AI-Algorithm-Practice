c00cf5603e5665458f07f85dbd11a601fb1702b8
nl1xx <96980019+nl1xx@users.noreply.github.com>
2025-04-29 20:00:22 +0800

    Add files via upload

--- 75DeepLab v3.py
+++ 75DeepLab v3.py
@@ -1,125 +1,378 @@
+# DeepLab v3 (ResNet50, ResNet101)
+# https://github.com/WZMIAOMIAO/deep-learning-for-image-processing/blob/master/pytorch_segmentation/deeplab_v3/src/deeplabv3_model.py
+
 import torch
-import torch.nn as nn
+from collections import OrderedDict
+from typing import Dict, List
+from torch import nn, Tensor
+from torch.nn import functional as F
 
 
-class VGG13(nn.Module):
-    def __init__(self):
-        super(VGG13, self).__init__()
-        self.stage_1 = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2),
-        )
+def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1):
+    """
+    3x3 convolution with padding
+    """
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=dilation, groups=groups, bias=False, dilation=dilation)
 
-        self.stage_2 = nn.Sequential(
-            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(128),
-            nn.ReLU(),
-            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(128),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2),
-        )
 
-        self.stage_3 = nn.Sequential(
-            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(),
-            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(),
-            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2),
-        )
+def conv1x1(in_planes, out_planes, stride=1):
+    """
+    1x1 convolution
+    """
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
 
-        self.stage_4 = nn.Sequential(
-            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(512),
-            nn.ReLU(),
-            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(512),
-            nn.ReLU(),
-            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(512),
-            nn.ReLU(),
-            nn.MaxPool2d(2, stride=1, padding=1),
-        )
 
-        self.stage_5 = nn.Sequential(
-            # 空洞卷积
-            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=2, dilation=2),
-            nn.BatchNorm2d(512),
-            nn.ReLU(),
-            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=2, dilation=2),
-            nn.BatchNorm2d(512),
-            nn.ReLU(),
-            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=2, dilation=2),
-            nn.BatchNorm2d(512),
-            nn.ReLU(),
-            nn.MaxPool2d(2, stride=1),
-        )
+# ResNet的基本构建块
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1, base_width=64, dilation=1, norm_layer=None):
+        super(Bottleneck, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        width = int(planes * (base_width / 64.)) * groups
+        # Both self.conv2 and self.downsample layers downsample the input when stride != 1
+        self.conv1 = conv1x1(inplanes, width)
+        self.bn1 = norm_layer(width)
+        self.conv2 = conv3x3(width, width, stride, groups, dilation)
+        self.bn2 = norm_layer(width)
+        self.conv3 = conv1x1(width, planes * self.expansion)
+        self.bn3 = norm_layer(planes * self.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
 
     def forward(self, x):
-        x = x.float()
-        x1 = self.stage_1(x)
-        x2 = self.stage_2(x1)
-        x3 = self.stage_3(x2)
-        x4 = self.stage_4(x3)
-        x5 = self.stage_5(x4)
-        return [x1, x2, x3, x4, x5]
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
 
 
-class ASPP_module(nn.ModuleList):
-    def __init__(self, in_channels, out_channels, dilation_list=[1, 6, 12, 18]):
-        super(ASPP_module, self).__init__()
-        self.dilation_list = dilation_list
-        for dia_rate in self.dilation_list:
-            self.append(
-                nn.Sequential(
-                    nn.Conv2d(in_channels, out_channels, kernel_size=1 if dia_rate == 1 else 3, dilation=dia_rate,
-                              padding=0 if dia_rate == 1 else dia_rate),
-                    nn.BatchNorm2d(out_channels),
-                    nn.ReLU()
-                )
+# 完整的ResNet架构
+class ResNet(nn.Module):
+    def __init__(self, block, layers, num_classes=1000, zero_init_residual=False, groups=1, width_per_group=64,
+                 replace_stride_with_dilation=None, norm_layer=None):
+        super(ResNet, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        self._norm_layer = norm_layer
+
+        self.inplanes = 64
+        self.dilation = 1
+        if replace_stride_with_dilation is None:
+            # each element in the tuple indicates if we should replace
+            # the 2x2 stride with a dilated convolution instead
+            replace_stride_with_dilation = [False, False, False]
+        if len(replace_stride_with_dilation) != 3:
+            raise ValueError("replace_stride_with_dilation should be None "
+                             "or a 3-element tuple, got {}".format(replace_stride_with_dilation))
+        self.groups = groups
+        self.base_width = width_per_group
+        self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=7, stride=2, padding=3,
+                               bias=False)
+        self.bn1 = norm_layer(self.inplanes)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2, dilate=replace_stride_with_dilation[0])
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2, dilate=replace_stride_with_dilation[1])
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2, dilate=replace_stride_with_dilation[2])
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+        # Zero-initialize the last BN in each residual branch,
+        # so that the residual branch starts with zeros, and each residual block behaves like an identity.
+        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
+        if zero_init_residual:
+            for m in self.modules():
+                if isinstance(m, Bottleneck):
+                    nn.init.constant_(m.bn3.weight, 0)
+
+    def _make_layer(self, block, planes, blocks, stride=1, dilate=False):
+        norm_layer = self._norm_layer
+        downsample = None
+        previous_dilation = self.dilation
+        if dilate:
+            self.dilation *= stride
+            stride = 1
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                conv1x1(self.inplanes, planes * block.expansion, stride),
+                norm_layer(planes * block.expansion),
             )
 
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample, self.groups, self.base_width, previous_dilation, norm_layer))
+        self.inplanes = planes * block.expansion
+        for _ in range(1, blocks):
+            layers.append(block(self.inplanes, planes, groups=self.groups, base_width=self.base_width, dilation=self.dilation, norm_layer=norm_layer))
+
+        return nn.Sequential(*layers)
+
+    def _forward_impl(self, x):
+        # See note [TorchScript super()]
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+
+        return x
+
     def forward(self, x):
-        outputs = []
-        for aspp_module in self:
-            outputs.append(aspp_module(x))
-        return torch.cat(outputs, 1)
+        return self._forward_impl(x)
 
 
+def _resnet(block, layers, **kwargs):
+    model = ResNet(block, layers, **kwargs)
+    return model
+
+
+def resnet50(**kwargs):
+    return _resnet(Bottleneck, [3, 4, 6, 3], **kwargs)
+
+
+def resnet101(**kwargs):
+    return _resnet(Bottleneck, [3, 4, 23, 3], **kwargs)
+
+
+# 从骨干网络中提取中间层特征
+class IntermediateLayerGetter(nn.ModuleDict):
+    _version = 2
+    __annotations__ = {
+        "return_layers": Dict[str, str],
+    }
+
+    def __init__(self, model: nn.Module, return_layers: Dict[str, str]) -> None:
+        if not set(return_layers).issubset([name for name, _ in model.named_children()]):
+            raise ValueError("return_layers are not present in model")
+        orig_return_layers = return_layers
+        return_layers = {str(k): str(v) for k, v in return_layers.items()}
+
+        # 重新构建backbone，将没有使用到的模块全部删掉
+        layers = OrderedDict()
+        for name, module in model.named_children():
+            layers[name] = module
+            if name in return_layers:
+                del return_layers[name]
+            if not return_layers:
+                break
+
+        super(IntermediateLayerGetter, self).__init__(layers)
+        self.return_layers = orig_return_layers
+
+    def forward(self, x: Tensor) -> Dict[str, Tensor]:
+        out = OrderedDict()
+        for name, module in self.items():
+            x = module(x)
+            if name in self.return_layers:
+                out_name = self.return_layers[name]
+                out[out_name] = x
+        return out
+
+
+# DeepLab v3
 class DeepLabV3(nn.Module):
-    def __init__(self, num_classes):
+    __constants__ = ['aux_classifier']
+
+    def __init__(self, backbone, classifier, aux_classifier=None):
         super(DeepLabV3, self).__init__()
-        self.num_classes = num_classes
-        self.ASPP_module = ASPP_module(512, 256, dilation_list=[1, 6, 12, 18])
-        self.backbone = VGG13()
-        self.final = nn.Sequential(
-            nn.Conv2d(256 * 4 + 256, 256, kernel_size=1),
+        self.backbone = backbone
+        self.classifier = classifier
+        self.aux_classifier = aux_classifier
+
+    def forward(self, x: Tensor) -> Dict[str, Tensor]:
+        input_shape = x.shape[-2:]
+        # contract: features is a dict of tensors
+        features = self.backbone(x)
+
+        result = OrderedDict()
+        x = features["out"]
+        x = self.classifier(x)
+        # 使用双线性插值还原回原图尺度
+        x = F.interpolate(x, size=input_shape, mode='bilinear', align_corners=False)
+        result["out"] = x
+
+        if self.aux_classifier is not None:
+            x = features["aux"]
+            x = self.aux_classifier(x)
+            # 使用双线性插值还原回原图尺度
+            x = F.interpolate(x, size=input_shape, mode='bilinear', align_corners=False)
+            result["aux"] = x
+
+        return result
+
+
+# 全卷积网络头部
+class FCNHead(nn.Sequential):
+    def __init__(self, in_channels, channels):
+        inter_channels = in_channels // 4
+        super(FCNHead, self).__init__(
+            nn.Conv2d(in_channels, inter_channels, 3, padding=1, bias=False),
+            nn.BatchNorm2d(inter_channels),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Conv2d(inter_channels, channels, 1)
+        )
+
+
+# ASPP模块的组成部分
+class ASPPConv(nn.Sequential):
+    def __init__(self, in_channels: int, out_channels: int, dilation: int) -> None:
+        super(ASPPConv, self).__init__(
+            nn.Conv2d(in_channels, out_channels, 3, padding=dilation, dilation=dilation, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU()
+        )
+
+
+# ASPP模块的组成部分
+class ASPPPooling(nn.Sequential):
+    def __init__(self, in_channels: int, out_channels: int) -> None:
+        super(ASPPPooling, self).__init__(
+            nn.AdaptiveAvgPool2d(1),
+            nn.Conv2d(in_channels, out_channels, 1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU()
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        size = x.shape[-2:]
+        for mod in self:
+            x = mod(x)
+        return F.interpolate(x, size=size, mode='bilinear', align_corners=False)
+
+
+# ASPP
+class ASPP(nn.Module):
+    def __init__(self, in_channels: int, atrous_rates: List[int], out_channels: int = 256) -> None:
+        super(ASPP, self).__init__()
+        modules = [
+            nn.Sequential(nn.Conv2d(in_channels, out_channels, 1, bias=False),
+                          nn.BatchNorm2d(out_channels),
+                          nn.ReLU())
+        ]
+
+        rates = tuple(atrous_rates)
+        for rate in rates:
+            modules.append(ASPPConv(in_channels, out_channels, rate))
+
+        modules.append(ASPPPooling(in_channels, out_channels))
+
+        self.convs = nn.ModuleList(modules)
+
+        self.project = nn.Sequential(
+            nn.Conv2d(len(self.convs) * out_channels, out_channels, 1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(),
+            nn.Dropout(0.5)
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        _res = []
+        for conv in self.convs:
+            _res.append(conv(x))
+        res = torch.cat(_res, dim=1)
+        return self.project(res)
+
+
+# 结合ASPP和卷积层的分类头部
+class DeepLabHead(nn.Sequential):
+    def __init__(self, in_channels: int, num_classes: int) -> None:
+        super(DeepLabHead, self).__init__(
+            ASPP(in_channels, [12, 24, 36]),
+            nn.Conv2d(256, 256, 3, padding=1, bias=False),
             nn.BatchNorm2d(256),
             nn.ReLU(),
-            nn.Conv2d(256, self.num_classes, kernel_size=1)
+            nn.Conv2d(256, num_classes, 1)
         )
-        self.avg_pool = nn.Sequential(
-            nn.AdaptiveAvgPool2d((1)),
-            nn.Conv2d(512, 256, 1, bias=False),
-            nn.BatchNorm2d(256),
-            nn.ReLU(inplace=True))
 
-    def forward(self, x):
-        x = self.backbone(x)[-1]
-        x_1 = self.ASPP_module(x)
-        x_2 = nn.functional.interpolate(self.avg_pool(x), size=(x.size(2), x.size(3)), mode='bilinear',
-                                        align_corners=True)
-        x = torch.cat([x_1, x_2], 1)
-        x = nn.functional.interpolate(input=x, scale_factor=8, mode='bilinear', align_corners=True)
-        x = self.final(x)
-        return x
+
+def deeplabv3_resnet50(aux, num_classes=21, pretrain_backbone=False):
+    # 'resnet50_imagenet': 'https://download.pytorch.org/models/resnet50-0676ba61.pth'
+    # 'deeplabv3_resnet50_coco': 'https://download.pytorch.org/models/deeplabv3_resnet50_coco-cd0a2569.pth'
+    backbone = resnet50(replace_stride_with_dilation=[False, True, True])
+
+    if pretrain_backbone:
+        # 载入resnet50 backbone预训练权重
+        backbone.load_state_dict(torch.load("resnet50.pth", map_location='cpu'))
+
+    out_inplanes = 2048
+    aux_inplanes = 1024
+
+    return_layers = {'layer4': 'out'}
+    if aux:
+        return_layers['layer3'] = 'aux'
+    backbone = IntermediateLayerGetter(backbone, return_layers=return_layers)
+
+    aux_classifier = None
+    # why using aux: https://github.com/pytorch/vision/issues/4292
+    if aux:
+        aux_classifier = FCNHead(aux_inplanes, num_classes)
+
+    classifier = DeepLabHead(out_inplanes, num_classes)
+
+    model = DeepLabV3(backbone, classifier, aux_classifier)
+
+    return model
+
+
+def deeplabv3_resnet101(aux, num_classes=21, pretrain_backbone=False):
+    # 'resnet101_imagenet': 'https://download.pytorch.org/models/resnet101-63fe2227.pth'
+    # 'deeplabv3_resnet101_coco': 'https://download.pytorch.org/models/deeplabv3_resnet101_coco-586e9e4e.pth'
+    backbone = resnet101(replace_stride_with_dilation=[False, True, True])
+
+    if pretrain_backbone:
+        # 载入resnet101 backbone预训练权重
+        backbone.load_state_dict(torch.load("resnet101.pth", map_location='cpu'))
+
+    out_inplanes = 2048
+    aux_inplanes = 1024
+
+    return_layers = {'layer4': 'out'}
+    if aux:
+        return_layers['layer3'] = 'aux'
+    backbone = IntermediateLayerGetter(backbone, return_layers=return_layers)
+
+    aux_classifier = None
+    # why using aux: https://github.com/pytorch/vision/issues/4292
+    if aux:
+        aux_classifier = FCNHead(aux_inplanes, num_classes)
+
+    classifier = DeepLabHead(out_inplanes, num_classes)
+
+    model = DeepLabV3(backbone, classifier, aux_classifier)
+
+    return model
